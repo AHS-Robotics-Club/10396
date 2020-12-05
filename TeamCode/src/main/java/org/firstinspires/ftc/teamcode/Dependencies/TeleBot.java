f1c4fae090548d0ba1b7ea4b2062993260293aad
Srihith Reddy <pallamreddysrihith@gmail.com>
2020-12-04 21:59:10 -0500

    Update TeleBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dependencies/TeleBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dependencies/TeleBot.java
@@ -4,13 +4,15 @@ import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.motors.CRServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class TeleBot {
     private Motor fL, fR, bL, bR, shooter, intake, lifter;
     private RevIMU imu;
-    private CRServo flick, grabber;
+    private CRServo flick;
+    private Servo grabber;
 
-    public TeleBot(Motor frontLeft, Motor frontRight, Motor backLeft, Motor backRight, Motor intakeParam, Motor shooterParam, Motor lift, CRServo grab, RevIMU imuParam, CRServo flicker) {
+    public TeleBot(Motor frontLeft, Motor frontRight, Motor backLeft, Motor backRight, Motor intakeParam, Motor shooterParam, Motor lift, Servo grab, RevIMU imuParam, CRServo flicker) {
         fL = frontLeft;
         fR = frontRight;
         bL = backLeft;
@@ -40,7 +42,7 @@ public class TeleBot {
         intake.set(0);
         lifter.set(0);
 
-        grabber.set(0);
+        grabber.setPosition(0.9);
         flick.set(0);
 
         fL.resetEncoder();
